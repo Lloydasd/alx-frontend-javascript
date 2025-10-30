89d8e0d27ca970a9f0011c0ff9c943c88db5f675
Benict <152749576+Benict@users.noreply.github.com>
2025-10-30 00:21:08 +0000

    Printing Teachers

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -27,3 +27,13 @@ const director1: Director = {
   fullTimeEmployee: true,
   numberOfReports: 17,
 };
+
+interface printTeacherFunction {
+  (firstName: string, lastName: string): string;
+}
+
+const printTeacher: printTeacherFunction = (firstName, lastName) => {
+  return `${firstName[0]}. ${lastName}`;
+};
+
+console.log(printTeacher('John','Doe'));
