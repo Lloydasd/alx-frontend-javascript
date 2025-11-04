c1ecce411fe4800ac42ca454c834f48832c13a12
Benict <152749576+Benict@users.noreply.github.com>
2025-11-04 18:28:35 +0000

    Fixed the issues with the Ambient Spacing

---
+++ 0x04-TypeScript/task_3/js/main.ts
@@ -0,0 +1,19 @@
+/// <reference path="./crud.d.ts" />
+
+import { CRUD } from './crud'
+
+import { RowID, RowElement }from './interface' 
+
+const row: RowElement ={
+  firstName : 'Lloyd',
+  lastName : 'Asiedu'
+}
+
+const newRowID:RowID = CRUD.insertRow(row)
+
+const updatedRow: RowElement = {...row,age:23}
+
+CRUD.updateRow(newRowID,updatedRow);
+
+
+CRUD.deleteRow(newRowID);
