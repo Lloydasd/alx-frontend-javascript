3ed41b52389fb216f7815d86f51d4f2e34086d0e
Benict <152749576+Benict@users.noreply.github.com>
2025-11-06 13:52:27 +0000

    Fixed checker issue

---
+++ 0x04-TypeScript/task_3/js/crud.d.ts
@@ -0,0 +1,9 @@
+/// <reference path="./interface.ts" />
+
+declare function insertRow(row: RowElement): RowID;
+
+declare function deleteRow(rowId: RowID): void;
+
+declare function updateRow(rowId: RowID, row:RowElement):RowID;
+
+
